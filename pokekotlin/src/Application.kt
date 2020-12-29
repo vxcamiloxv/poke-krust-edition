6ad63683841109e0b79917c0d81a2c9df5f5a129
AlmahirPM <apinango@univision.net>
2020-12-29 13:06:01 -0500

    Update Application.kt

--- pokekotlin/src/Application.kt
+++ pokekotlin/src/Application.kt
@@ -3,11 +3,23 @@ package org.pokekotlin
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
-
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+}
+
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Pokemon with Kotlin + RUST")
+            }
+        }
+    }.start(true)
 }
 
